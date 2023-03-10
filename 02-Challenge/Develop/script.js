ce39b029da9fa4ba001143567d8b7c181ca231b5
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-03-10 15:42:47 -0700

    added passwordlength

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -3,6 +3,9 @@ var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var password = [];
+var passwordLength = 0;
+var passwordLength = prompt("How many characters would you like your password to be? (8-128)");
+var passwordLength = parseInt(passwordLength);
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
